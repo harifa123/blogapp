9e766a98fe4bdc30dcc7e8b23b1886ba86d5b5bd
harifa123 <harifarifu@gmail.com>
2024-02-09 11:30:41 +0530

    msg

--- controller/blogRouter.js
+++ controller/blogRouter.js
@@ -1,13 +1,27 @@
 const express=require("express")
 const blogmodel=require("../model/blogmodel")
 const route=express.Router()
+const bcrypt=require("bcryptjs")
+hashpasswordgenerator=async(pass)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
 
 route.post("/signin",async(req,res)=>{
-    let data=req.body
-    let blog=new blogmodel(data)
-    let result=blog.save()
+    let {data}={"data":req.body}
+    let password=data.password
+    hashpasswordgenerator(password).then(
+        (hashedpassword)=>{
+            console.log(hashedpassword)
+            data.password=hashedpassword
+            console.log(data)
+            let blog=new blogmodel(data)
+            let result=blog.save()
 
-    res.json({status:"success"})
+            res.json({status:"success"})
+        }
+    )
+    
 
 })
 
